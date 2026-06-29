3b99c957e022604f010aceddf789eede7cbe3155
daarkyytb76-droid <daarkyytb76@gmail.com>
2026-06-29 12:21:21 +0200

    cript.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<body>
-<pre>
 local player = game.Players.LocalPlayer
 
 local gui = Instance.new("ScreenGui")
@@ -17,9 +13,13 @@ local messages = {
 	"T UN CLOCHARD ?"
 }
 
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://9118823105"
+sound.Volume = 2
+sound.Parent = gui
+
 for i = 1, 1000 do
 	task.spawn(function()
-
 		local frame = Instance.new("Frame")
 		frame.Size = UDim2.new(0, 250, 0, 120)
 		frame.Position = UDim2.new(
@@ -32,31 +32,14 @@ for i = 1, 1000 do
 		frame.BorderSizePixel = 2
 		frame.Parent = gui
 
-		-- BARRE HAUT
-		local topBar = Instance.new("Frame")
-		topBar.Size = UDim2.new(1, 0, 0, 30)
-		topBar.BackgroundColor3 = Color3.fromRGB(0, 120, 255)
-		topBar.Parent = frame
-
 		local title = Instance.new("TextLabel")
-		title.Size = UDim2.new(1, -30, 1, 0)
-		title.BackgroundTransparency = 1
+		title.Size = UDim2.new(1, 0, 0, 30)
+		title.BackgroundColor3 = Color3.fromRGB(0, 120, 255)
 		title.TextColor3 = Color3.new(1, 1, 1)
 		title.TextScaled = true
 		title.Text = "popup"
-		title.Parent = topBar
+		title.Parent = frame
 
-		-- BOUTON FERMER ❌
-		local closeBtn = Instance.new("TextButton")
-		closeBtn.Size = UDim2.new(0, 30, 1, 0)
-		closeBtn.Position = UDim2.new(1, -30, 0, 0)
-		closeBtn.BackgroundTransparency = 1
-		closeBtn.Text = "❌"
-		closeBtn.TextColor3 = Color3.new(1, 1, 1)
-		closeBtn.TextScaled = true
-		closeBtn.Parent = topBar
-
-		-- TEXTE
 		local text = Instance.new("TextLabel")
 		text.Size = UDim2.new(1, -20, 1, -50)
 		text.Position = UDim2.new(0, 10, 0, 40)
@@ -66,24 +49,8 @@ for i = 1, 1000 do
 		text.Text = messages[((i - 1) % #messages) + 1]
 		text.Parent = frame
 
-		-- SON
-		local sound = Instance.new("Sound")
-		sound.SoundId = "rbxassetid://138081500"
-		sound.Volume = 2
-		sound.Parent = frame
 		sound:Play()
-
-		game:GetService("Debris"):AddItem(sound, 2)
-
-		-- FERMETURE POPUP
-		closeBtn.MouseButton1Click:Connect(function()
-			frame:Destroy()
-		end)
-
 	end)
 
 	task.wait(0.1)
 end
-</pre>
-</body>
-</html>
